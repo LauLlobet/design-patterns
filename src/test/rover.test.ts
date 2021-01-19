71e3fdd828b78f32f88023bf9c5e7bdef7365c54
Lau Llobet <lau.llobet@mypulse.ai>
2021-01-19 11:43:15 +0100

    🟢🟢🟢🟢 GREEN - Test that the implementation is generic as expected - to squash

--- src/test/rover.test.ts
+++ src/test/rover.test.ts
@@ -31,6 +31,8 @@ describe("Mars Rover", () => {
   it.each([
     ["MU", "0:0:N"],
     ["MMU", "0:1:N"],
+    ["MMMU", "0:2:N"],
+    ["MMMUU", "0:1:N"],
   ])("when given %s it should return %s after undoing", (input, output) => {
     const rovercli = new RoverCLI();
     rovercli.execute(input);
